4a5827bee07b969b90ed3708652534fa73ecb1ea
trashcluster <33961608+trashcluster@users.noreply.github.com>
2018-10-05 10:01:41 +0200

    TD4 fin de seance

--- TD4.py
+++ TD4.py
@@ -1,14 +1,39 @@
-#programme horaire_des_vols
-def tracer_un_trait() :
-	print(' ')
-	print('-------------------------------------------------')
-	print(' ')
+#Vol vers
 
-tracer_un_trait()
-print('Le vol en direction de Pekin décollera à  9h00')
-tracer_un_trait()
-print('Le vol en direction de Bucarest décollera à  9h30')
-tracer_un_trait()
-print('Le vol en direction de Istanbul décollera à  9h45 ')
-tracer_un_trait()
+def vol_vers(destination, horaire) :
+	print(' ')
+	print('Le vol en direction de ' + destination + ' dÃ©collera Ã  ' + horaire)
+	print(' ')
+	print('-----------------------------------------------------------')
 
+vol_vers('Pekin', '9h00')
+vol_vers('Paris', '16h54')
+vol_vers('Istanbul', '7h39')
+
+#Argus1
+
+def argus1(prix) :
+	remise1 = int(20)
+	remise2 = int(13)
+	remise3 = int(8)
+	print('Prix initiale', prix)
+	prix = prix-prix*remise1/100
+	print("Prix au bout d'un an avec remise de ", remise1, "% : ", prix)
+	prix = prix-prix*remise2/100
+	print("Prix au bout de 2 ans avec remise de ", remise2, "% : ", prix)
+	prix = prix-prix*remise3/100
+	print("Prix au bout de 3 ans avec remise de ", remise3, "% : ", prix)
+argus1(int(input("Entre le prix du vehicule")))
+
+
+#Argus2
+
+def argus1(prix, remise1, remise2, remise3) :
+	print('Prix initiale', prix)
+	prix = prix-prix*remise1/100
+	print("Prix au bout d'un an avec remise de ", remise1, "% : ", prix)
+	prix = prix-prix*remise2/100
+	print("Prix au bout de 2 ans avec remise de ", remise2, "% : ", prix)
+	prix = prix-prix*remise3/100
+	print("Prix au bout de 3 ans avec remise de ", remise3, "% : ", prix)
+argus1(input("Entre le prix du vehicule suivi des remises sur 3 ans (3 valeures)"))
