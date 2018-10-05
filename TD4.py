eed165b3a559936f6e90babb29c7a0a98dde2d8f
trashcluster <33961608+trashcluster@users.noreply.github.com>
2018-10-05 09:22:53 +0200

    TD4

---
+++ TD4.py
@@ -0,0 +1,14 @@
+#programme horaire_des_vols
+def tracer_un_trait() :
+	print(' ')
+	print('-------------------------------------------------')
+	print(' ')
+
+tracer_un_trait()
+print('Le vol en direction de Pekin décollera à  9h00')
+tracer_un_trait()
+print('Le vol en direction de Bucarest décollera à  9h30')
+tracer_un_trait()
+print('Le vol en direction de Istanbul décollera à  9h45 ')
+tracer_un_trait()
+
