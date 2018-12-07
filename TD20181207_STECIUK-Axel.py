499866cc4c675e56be202d91fa1c1859463049cf
trashcluster <33961608+trashcluster@users.noreply.github.com>
2018-12-07 09:56:08 +0100

    TP 07 DEC 2018

---
+++ TD20181207_STECIUK-Axel.py
@@ -0,0 +1,36 @@
+#TD du 07 Decembre 2018 9h-10h
+#Suite de Syracuse
+#10 premiers nombres = U 0  =  1000
+#U 1  =  500
+#U 2  =  250
+#U 3  =  125
+#U 4  =  376
+#U 5  =  188
+#U 6  =  94
+#U 7  =  47
+#U 8  =  142
+#U 9  =  71
+#U 10  =  214
+syr=int(input("Antre un nombre à calculer : "))
+u=0
+umax=0
+print("U",u," = ",int(syr))
+while syr > 1 :
+    if syr%2 == 0 :
+        syr/=2
+    else :
+        syr=syr*3+1
+    u+=1
+    if umax < syr :
+        umax = syr
+
+    print(int(syr))
+print("U fin =",u,"U max =",int(umax))
+
+# Jeu de l'oie
+from random import *
+pos=0
+while pos!=66 :
+    des=6*random()+6*random()
+    pos+=def mname(self, arg):
+        pass
