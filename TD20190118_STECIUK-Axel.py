8810f5d86c6fe429d928891c3ae26bae345a27ce
trashcluster <info@trashcluster.com>
2019-01-18 10:30:53 +0100

    Create TD20190118_STECIUK-Axel.py
    18/01/2018 Premiere partie

---
+++ TD20190118_STECIUK-Axel.py
@@ -0,0 +1,31 @@
+# cadavre exquis
+from random import *
+
+def A():
+    return int(5*random());
+
+lieu=['chez lui','à paris','sur un pont','sur la banquise','à la maison blanche']
+nom=['Benjamin','Paul','Olivier','Marianne','trump']
+verbe=['mange','asseoit','peint','prend','dirige']
+adverbe=['rapidement','lentement','jamais','calmement','gentillement']
+mot=['lapin','mouette','mug','voiture','pays']
+adjectif=['bleu','long','noble','parleur','noir']
+nombre=int(10*random())
+print(lieu[A()],nom[A()],verbe[A()],adverbe[A()],nombre,mot[A()],adjectif[A()])
+
+#Maximum
+tabl=[]
+maxCount=int(0)
+maxNumA=int(0)
+maxNum=int(0)
+for i in range(8):
+    x=int(input("Entre un nombre : "))
+    tabl.append(x)
+for i in tabl :
+    maxNumA = maxNum
+    if maxNum < tabl[i] :
+        maxNum=tabl[i]
+        maxCount=1
+    if maxNumA == maxNum :
+        maxCount+=1
+print("Maximum :", maxNum,"Nombre d'occurences :", maxCount)
