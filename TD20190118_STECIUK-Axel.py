7cbf22a9cf81598e5b94d755d95b69159752243a
trashcluster <info@trashcluster.com>
2019-01-21 09:35:37 +0100

    Update TD20190118_STECIUK-Axel.py

--- TD20190118_STECIUK-Axel.py
+++ TD20190118_STECIUK-Axel.py
@@ -14,21 +14,43 @@ nombre=int(10*random())
 print(lieu[A()],nom[A()],verbe[A()],adverbe[A()],nombre,mot[A()],adjectif[A()])
 
 #Maximum
-tabl=[]
-maxCount=int(0)
-maxNumA=int(0)
-maxNum=int(0)
-for i in range(8):
-    x=int(input("Entre un nombre : "))
-    tabl.append(x)
-for i in tabl :
-    maxNumA = maxNum
-    if maxNum < tabl[i] :
-        maxNum=tabl[i]
-        maxCount=1
-    if maxNumA == maxNum :
-        maxCount+=1
-print("Maximum :", maxNum,"Nombre d'occurences :", maxCount)
+#tabl=[]
+#maxCount=int(0)
+#maxNumA=int(0)
+#maxNum=int(0)
+#for i in range(8):
+#    x=int(input("Entre un nombre : "))
+#    tabl.append(x)
+#for i in tabl :
+#    maxNumA = maxNum
+#    if maxNum < tabl[i] :
+#        maxNum=tabl[i]
+#        maxCount=1
+#    if maxNumA == maxNum :
+#        maxCount+=1
+#print("Maximum :", maxNum,"Nombre d'occurences :", maxCount)
 
 # loto
-tirage=[]
+def tirer():
+    num=int()
+    global tirage
+    tirage=[]
+    num=int(49*random()+1)
+    tirage.append(num)
+    for i in range(4) :
+        while num in tirage :
+            num=int(49*random()+1)
+        tirage.append(num)
+        return tirage
+
+loto=[]
+juste=int()
+for i in range(5) :
+    x=int(input("Entre un numero : "))
+    loto.append(x)
+for j in range(10) :
+    tirer()
+    print(loto, tirage)
+    if loto == tirage :
+        juste+=1
+print("Nombre de fois où votre tirage est passé : ",juste)
